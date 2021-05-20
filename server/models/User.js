e29863a41a455be7a9fa90bf8428e075613f55fc
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-05-21 02:12:09 +0900

    회원가입

--- server/models/User.js
+++ server/models/User.js
@@ -27,4 +27,24 @@ const userSchema = mongoose.Schema({
     tokenExp : {
         type : Number
     }
+});
+
+userSchema.pre('save', function(next){
+    // email, password,name
+    var user = this;
+    console.log("user = ",user)
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                // 맨 처음에 들어오는 password는 plain password 해싱을 해줘야해요.
+                if(err) return next(err); // 에러 있으면 에러를 다음 함수로 넘겨준다는 뜻
+                user.password = hash;
+                next();
+            })
+        })
+    }else{
+        next()
+    }
 })
+
+module.exports = mongoose.model('User', userSchema)
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,22 @@
 
+const mongoose = require("mongoose"),
+User = require("../models/User")
 
 module.exports = (app) => {
-    app.get("/user", ()=>{
+    // ============ 회원가입 ============//
+    app.post("/user", (req, res)=>{
+        const user = new User({name : "name", password : "1234", email : "imsiemai@email.com"});
+        user.save((err, doc) => {
+            if(err) return res.json({ success : false, err : err.message })
+            return res.status(200).json({
+                success : true
+            })
+        })
         console.log("here")
-    })
+    });
+    // ============ 회원정보 가져오기 ============//
+    app.get("/user", (req,res) => {
+        console.log("/user 요청 들어옴")
+    });
+    
 }
